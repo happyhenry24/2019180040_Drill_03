988757fb88d028d7f764740215632d0e82b16eea
happyhenry24 <joyongcheon24@gmail.com>
2024-09-24 12:33:51 +0900

    윗줄 이동 완성

--- 2/TEST.py
+++ 2/TEST.py
@@ -6,22 +6,24 @@ open_canvas()
 grass = load_image('grass.png')
 boy = load_image('character.png')
 
+def draw_boy(x, y):
+    clear_canvas_now()
+    boy.draw_now(x, y)
+    delay(0.1)
+
 def run_circle():
     print('CIRCLE')
-
-    
     r, cx, cy = 300, 800//2, 600//2
     for degree in range(0, 360, 3):
         theta = math.radians(degree)
         x = r * math.cos(theta) + cx
         y = r * math.sin(theta) + cy
-        
-        clear_canvas_now()
-        boy.draw_now(x,y)
-        delay(0.1)
+        draw_bow(x,y)
 
 def run_top():
     print('TOP')
+    for x in range(0, 800, 10):
+        draw_boy(x, 550)
     pass
 
 def run_right():
