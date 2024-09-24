152197b3a48f6d060751de74af8403e0045713b4
happyhenry24 <joyongcheon24@gmail.com>
2024-09-24 11:49:46 +0900

    뼈대 완성(프린트로 무한 루프 구조 확인)

--- 2/TEST.py
+++ 2/TEST.py
@@ -1,39 +1,22 @@
 from pico2d import *
-from math import *
 
 open_canvas()
 
 grass = load_image('grass.png')
-character = load_image('character.png')
+boy = load_image('character.png')
 
-x = 0
-y = 0
-while(x < 790):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 90)
-    x += 2
-    delay(0.01)
+def run_circle():
+    print('CIRCLE')
+    pass
+
+def run_rectangle():
+    print('RECTANGLE')
+    pass
     
-while(y < 580):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(790, y)
-    y += 2
-    delay(0.01)
 
-while(x > 10):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(x, 580)
-    x -= 2
-    delay(0.01)
-
-while(y > 90):
-    clear_canvas_now()
-    grass.draw_now(400, 30)
-    character.draw_now(0, y)
-    y -= 2
-    delay(0.01)
+while True:
+    run_circle()
+    run_rectangle()
+    
 close_canvas()
 
