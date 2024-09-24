7085711bd7a648036882aa9aa7533a8ca7c65bcb
happyhenry24 <joyongcheon24@gmail.com>
2024-09-24 12:27:21 +0900

    사각운동 기본 흐름구조 완성

--- 2/TEST.py
+++ 2/TEST.py
@@ -19,16 +19,35 @@ def run_circle():
         clear_canvas_now()
         boy.draw_now(x,y)
         delay(0.1)
-    
+
+def run_top():
+    print('TOP')
+    pass
+
+def run_right():
+    print('RIGHT')
+    pass
+
+def run_bottom():
+    print('BOTTOM')
+    pass
+
+def run_left():
+    print('LEFT')
     pass
 
 def run_rectangle():
     print('RECTANGLE')
+    run_top()
+    run_right()
+    run_bottom()
+    run_left()
+    
     pass
     
 
 while True:
-    run_circle()
+    #run_circle()
     run_rectangle()
     break
     
